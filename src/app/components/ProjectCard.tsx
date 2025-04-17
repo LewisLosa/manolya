6b952366e7cf117ddd3ac0aa926d637a1980fa45
Eyüp Şengöz <lewislosa@gmail.com>
2025-04-17 23:37:37 +0300

    Proje komponenti eklendi.

---
+++ src/app/components/ProjectCard.tsx
@@ -0,0 +1,149 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheckCircle,
+  faFileLines,
+  faCodeBranch,
+  faFilePen,
+  faLaptop,
+  faGaugeHigh,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FaSymbol, IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface Project {
+  imageUrl: string;
+  altText: string;
+  isNew?: boolean;
+  isDeveloping?: boolean;
+  title: string;
+  features: string[];
+  featureIcons: IconProp[];
+  viewLink?: string;
+  githubLink?: string;
+}
+
+const ProjectCard: React.FC<Project> = ({
+  imageUrl,
+  altText,
+  isNew,
+  isDeveloping,
+  title,
+  features,
+  featureIcons,
+  viewLink,
+  githubLink,
+}) => {
+  return (
+    <div className="rounded-3xl overflow-hidden flex flex-col md:flex-row mb-8">
+      <div className="relative w-full md:w-1/2">
+        <img
+          src={imageUrl}
+          alt={altText}
+          className="w-128 h-full object-cover rounded-3xl"
+        />
+      </div>
+      <div className="p-8 flex-1">
+        <h3 className="text-3xl font-normal mb-4">{title}</h3>
+        <ul className=" space-y-3 mb-6">
+          {features.map((feature, index) => {
+            return (
+              <li className="flex items-center text-lg" key={index}>
+                <FontAwesomeIcon
+                  icon={featureIcons[index]}
+                  className=" mr-3 fa-fw"
+                />
+                {feature}
+              </li>
+            );
+          })}
+        </ul>
+        <div className="flex gap-4 mt-4">
+          {viewLink && (
+            <a
+              href={viewLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="py-2 px-6 rounded-full text-lg text-blue-500 flex items-center group relative overflow-hidden transition-all duration-300 hover:pr-10"
+            >
+              <FontAwesomeIcon icon={faFileLines} className="mr-2" />
+              Gözat
+              <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                className="absolute right-0 opacity-0 transform translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 mr-3"
+              />
+            </a>
+          )}
+          {githubLink && (
+            <a
+              href={githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="dark:border-white py-2 px-6 rounded-full text-lg flex items-center group relative overflow-hidden transition-all duration-300 hover:pr-10"
+            >
+              <FontAwesomeIcon icon={faGithub} className="mr-2" />
+              Github
+              <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                className="absolute right-0 opacity-0 transform translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 mr-3"
+              />
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ProjelerSection: React.FC = () => {
+  const projectsData: Project[] = [
+    {
+      imageUrl: "/imgs/setscript.png",
+      altText: "Setscript.com masaüstü uygulaması",
+      isNew: true,
+      isDeveloping: true,
+      title: "Setscript.com için masaüstü uygulaması",
+      features: [
+        "Çevrimdışı kullanım",
+        "Açık kaynaklı",
+        "Çapraz platform",
+        "Performanslı",
+      ],
+      featureIcons: [faCheckCircle, faFilePen, faLaptop, faGaugeHigh],
+      viewLink: "#",
+      githubLink: "#",
+    },
+    {
+      imageUrl: "/imgs/duru.png",
+      altText: "Duru - Hotel Yönetim Otomasyonu",
+      isNew: true,
+      isDeveloping: true,
+      title: "Duru -- Hotel Yönetim Otomasyonu",
+      features: [
+        "Çevrimdışı kullanım",
+        "Açık kaynaklı",
+        "Kullanıcı Dostu Arayüz",
+        "Performanslı",
+      ],
+      featureIcons: [faCheckCircle, faFilePen, faLaptop, faGaugeHigh],
+      viewLink: "#",
+      githubLink: "#",
+    },
+  ];
+
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <div className=" mx-auto px-4">
+        <h2 className="text-4xl font-normal mb-12">Projelerim</h2>
+        <div className="space-y-12">
+          {projectsData.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjelerSection;
