9904fa64348dba71cdf05ec815381f45ba03b5f7
Eyüp Şengöz <lewislosa@gmail.com>
2025-04-18 15:51:42 +0300

    Karanlık mod desteği iyileştirildi.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,19 +8,19 @@ const Navbar = () => {
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
 
         {/* Sol Taraf - Logo */}
-        <Link href="/" className="text-2xl font-bold text-gray-800 hover:text-gray-700">
+        <Link href="/" className="text-2xl font-bold text-gray-800 hover:text-gray-700 dark:text-gray-50 dark:hover:text-gray-400">
           losa.
         </Link>
 
         {/* Sağ Taraf - Menü Linkleri */}
         <div className="flex items-center space-x-6">
-          <Link href="/projeler" className="text-gray-600 hover:text-gray-900">
+          <Link href="/projeler" className="text-gray-600 hover:text-gray-900 dark:text-gray-50 dark:hover:text-gray-400">
             Projeler
           </Link>
-          <Link href="/about" className="text-gray-600 hover:text-gray-900">
+          <Link href="/about" className="text-gray-600 hover:text-gray-900 dark:text-gray-50 dark:hover:text-gray-400">
             Hakkımda
           </Link>
-          <Link href="https://drive.google.com/file/d/1nkFktkrbYJ1-rbCNUmAzAy1BGN5pdLFb/view?usp=sharing" target='_blank' className="text-gray-600 hover:text-gray-900">
+          <Link href="https://drive.google.com/file/d/1nkFktkrbYJ1-rbCNUmAzAy1BGN5pdLFb/view?usp=sharing" target='_blank' className="text-gray-600 hover:text-gray-900 dark:text-gray-50 dark:hover:text-gray-400">
              CV
           </Link>
         </div>
