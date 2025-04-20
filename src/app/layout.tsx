356dd2a03ba0446ebdbbe6276482dd2903071b9f
Eyüp Şengöz <lewislosa@gmail.com>
2025-04-20 20:24:40 +0300

    Kod yeniden faktörlendi.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 const productSans = localFont({
   src: [
     {
-      path: '../../public/fonts/product-sans/Product Sans Regular.ttf',
-      weight: '400',
-      style: 'normal',
+      path: "../../public/fonts/product-sans/Product Sans Regular.ttf",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../../public/fonts/product-sans/Product Sans Italic.ttf',
-      weight: '400',
-      style: 'italic',
+      path: "../../public/fonts/product-sans/Product Sans Italic.ttf",
+      weight: "400",
+      style: "italic",
     },
     {
-      path: '../../public/fonts/product-sans/Product Sans Bold.ttf',
-      weight: '700',
-      style: 'normal',
+      path: "../../public/fonts/product-sans/Product Sans Bold.ttf",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: '../../public/fonts/product-sans/Product Sans Bold Italic.ttf',
-      weight: '700',
-      style: 'italic',
+      path: "../../public/fonts/product-sans/Product Sans Bold Italic.ttf",
+      weight: "700",
+      style: "italic",
     },
   ],
-})
+});
 export const metadata: Metadata = {
   title: "Eyüp Şengöz - Portfolio",
   description: "Eyüp Şengöz'ün kişisel portfolyo sitesi",
@@ -41,9 +41,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
-      <body className={`${productSans.className} antialiased`}>
-        {children}
-      </body>
+      <body className={`${productSans.className} antialiased`}>{children}</body>
     </html>
   );
 }
