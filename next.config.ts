1b3bfa0cf57b8223ba3bc9e5b15ec6736c414bdc
LewisLosa <lewislosa@gmail.com>
2026-01-11 01:01:41 +0300

    chore: /nixos-install route added.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
 import type { NextConfig } from "next";
+import { redirect } from "next/navigation";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/nixos-install',
+        destination: 'https://raw.githubusercontent.com/LewisLosa/flake/refs/heads/main/install.sh',
+        permanent: false, // 302 redirect
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
